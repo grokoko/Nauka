12d3461f5dc7cfd520eaa713defd36d6e380ba2d
Konrad Lippert <45361699+grokoko@users.noreply.github.com>
2018-12-06 16:37:02 +0100

    Fixy
    Działa teraz dokladnie, przy dodawaniu i odejmowania punktów sprawdza czy łączna ilość się zgadza.

--- Rozdział 5/Kreator postaci.py
+++ Rozdział 5/Kreator postaci.py
@@ -3,8 +3,11 @@
 #między 4ry atrybuty: siła, zdrowie, mądrość, żręczność.
 #Gracz może również odejmować pkt z powrotem do puli
 
-atrybuty = []
 points = 30
+sila = 0
+zdrowie = 0
+madrosc = 0
+zrecznosc = 0
 choice = None
 while choice != "0":
 
@@ -15,8 +18,10 @@ while choice != "0":
     cechy: sila, zdrowie, mądrość, zręczność.
     0 - zakończ
     1 - Wyświetl atrybuty
-    2 - Dodaj punkty
-    3 - Odejmij punkty
+    2 - Zmień siłę
+    3 - Zmień zdrowie
+    4 - Zmień mądrość
+    5 - Zmień zręczność
     """
     )
 
@@ -25,37 +30,106 @@ while choice != "0":
 
     # zakończ
     if choice == "0":
-        print("Do widzenia,")
+        print("Do widzenia.")
 
     # Wyświetl wszystkie atrybuty
     elif choice == "1":
         print("Pozostałe punkty: ", points, "\n")
-        print("Atrybuty")
-        print("Cecha\tWartość")
-        for entry in atrybuty:
-            cecha, wartosc = entry
-            print(cecha, "\t", wartosc)
+        print("Atrybuty:\nSiła:", sila, "\nZdrowie:", zdrowie, \
+              "\nMądrość:", madrosc, "\nZręczność:", zrecznosc)
             
-    # Dodaj punkty
+    # SIŁA !
     elif choice == "2":
-        if points > 0:
+        if points >= 0 and points <= 30:
             print("Pozostałe punkty: ", points)
-            cecha = input("Podaj cechę: ")
-            wartosc = int(input("Ile punktów chcesz dodać do tej cechy?"))
-            entry = (cecha, wartosc)
-            atrybuty.append(entry)
-            points -= wartosc
+            choice = input("Wpisz '+' jeśli chcesz dodać punkty lub '-' jeśli odjąć:")
+            if choice == "+":
+                punkty = int(input("Ile punktów chcesz dodać?"))
+                if punkty > points or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna liczba pkt to:", points)
+                else:
+                    sila += punkty
+                    points -= punkty
+            elif choice == "-":
+                punkty = int(input("Ile punktów chcesz odjąć?"))
+                if punkty > sila or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna wartość siły to:", sila)
+                else:
+                    sila -= punkty
+                    points += punkty
         else:
             print("Za mało punktów!")
-        
-    # Odejmij punkty
+            choice = input("Wybierasz:")                  
+                             
+    # Zdrowie
     elif choice == "3":
-        entry = input("Którą cechę chcesz usunąć?")
-        if entry in atrybuty:
-            atrybuty.remove(entry)
+        if points >= 0 and points <= 30:
+            print("Pozostałe punkty: ", points)
+            choice = input("Wpisz '+' jeśli chcesz dodać punkty lub '-' jeśli odjąć:")
+            if choice == "+":
+                punkty == int(input("Ile punktów chcesz dodać?"))
+                if punkty > points or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna liczba pkt to:", points)
+                else:
+                    zdrowie += punkty
+                    points -= punkty
+            elif choice == "-":
+                punkty = int(input("Ile punktów chcesz odjąć?"))
+                if punkty > zdrowie or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna wartość zdrowia to:", zdrowie)
+                else:
+                    zdrowie -= punkty
+                    points += punkty
         else:
-            print(entry, "Nie ma takiej cechy")
-            
+            print("Za mało punktów!")
+            choice = input("Wybierasz:")    
+                             
+    #Mądrość
+    elif choice == "4":
+        if points >= 0 and points <= 30:
+            print("Pozostałe punkty: ", points)
+            choice = input("Wpisz '+' jeśli chcesz dodać punkty lub '-' jeśli odjąć:")
+            if choice == "+":
+                punkty = int(input("Ile punktów chcesz dodać?"))
+                if punkty > points or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna liczba pkt to:", points)
+                else:
+                    madrosc += punkty
+                    points -= punkty
+            elif choice == "-":
+                punkty = int(input("Ile punktów chcesz odjąć?"))
+                if punkty > madrosc or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna wartość zdrowia to:", madrosc)
+                else:
+                    madrosc -= punkty
+                    points += punkty
+        else:
+            print("Za mało punktów!")
+            choice = input("Wybierasz:")    
+
+    #Zręczność
+    elif choice == "5":
+        if points >= 0 and points <= 30:
+            print("Pozostałe punkty: ", points)
+            choice = input("Wpisz '+' jeśli chcesz dodać punkty lub '-' jeśli odjąć:")
+            if choice == "+":
+                punkty = int(input("Ile punktów chcesz dodać?"))
+                if punkty > points or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna liczba pkt to:", points)
+                else:
+                    zrecznosc += punkty
+                    points -= punkty
+            elif choice == "-":
+                punkty = int(input("Ile punktów chcesz odjąć?"))
+                if punkty > zrecznosc or punkty <= 0:
+                    print("Podałeś złą wartość. Aktualna wartość zdrowia to:", zrecznosc)
+                else:
+                    zrecznosc -= punkty
+                    points += punkty
+        else:
+            print("Za mało punktów!")
+            choice = input("Wybierasz:")      
+                             
     # nieznana opcja
     else:
         print("Niestety,", choice, "nie jest prawidłowym wyborem.")
