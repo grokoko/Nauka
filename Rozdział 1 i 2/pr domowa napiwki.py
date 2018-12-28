0b4a2606fbb03de647a570da92a300bd499b4486
Konrad Lippert <45361699+grokoko@users.noreply.github.com>
2018-12-28 18:50:42 +0100

    Rozdział 1 i 2

---
+++ Rozdział 1 i 2/pr domowa napiwki.py
@@ -0,0 +1,10 @@
+# Obliczenie 15% i 20% napiwku od sumy wpisanej przez użytkownika
+
+print("Obliczenie 15% i 20% napiwku od sumy wpisanej przez użytkownika")
+
+napiwek = int(input("Wysokość rachunku: "))
+
+print("15% napiwku od wysokości opłaty to: ", napiwek / 20 * 3)
+print("20% napiwku od wysykości opłaty to: ", napiwek / 5)
+
+input("\n\nEnter")
---
+++ Rozdział 1 i 2/pr domowa przysmaki.py
@@ -0,0 +1,14 @@
+# Nazwy zmiennych - nazwy zmiennych mogą się składać z liter, cyfr i podkreślnika _,
+# aczkolwiek nie może się zaczynać od cyfry.
+# Przykłady prawidłowych zmiennych:
+# name, surname, car1, car_2, car3_and_car4
+
+# Dwa przysmaki wybrane przez użytkownika i połączenie ich
+
+
+pierwszy = input("Pierwszy przysmak: ")
+drugi = input("Drugi przysmak: ")
+
+print("Twoje dwa przysmaki razem to:", pierwszy + drugi)
+
+input("\n\nEnter")
