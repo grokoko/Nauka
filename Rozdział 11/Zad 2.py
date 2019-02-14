f10189ae3f68f357e66003411859c76b0993712b
Konrad Lippert <45361699+grokoko@users.noreply.github.com>
2019-02-14 15:41:34 +0100

    Add files via upload

--- Rozdział 11/Zad 2.py
+++ Rozdział 11/Zad 2.py
@@ -3,21 +3,18 @@
 
 from superwires import games, color
 import random
+
 games.init(screen_width = 640, screen_height = 480, fps = 50)
 
 class Player(games.Sprite):
     """ Ludzik sterowany przez gracza """
-    image = games.load_image("ludzik.jpg")
+    image = games.load_image("ludzik.bmp")
 
     def __init__(self):
         """ Inicjalizuj gracza i licznik puntków """
         super(Player, self).__init__(image = Player.image,
                                     x = games.mouse.x,
                                     bottom = games.screen.height)
-
-        self.score = games.Text(value = 0, size = 25, color = color.black,
-                                top = 5, right = games.screen.width - 10)
-        games.screen.add(self.score)
 
     def update(self):
         """ Zmień pozycję na wyznaczoną przez współrzędną X myszy """
@@ -27,48 +24,50 @@ class Player(games.Sprite):
             self.left = 0
         if self.right > games.screen.width:
             self.right = games.screen.width
+        self.collision()
 
     def destroy(self):
         self.destroy()
-
-    def add_points(self):
-        self.score.value += 10
-        self.score.right = games.screen.width - 10
-
-
-class Stone(games.Sprite):
-    """ Kamień który spada na ziemię """
-
-    image = games.load_image("kamien.jpg")
-    speed = 2
-
-    def __init__(self, x = random.randrange(0, games.screen.width), y = 0):
-        super(Stone, self).__init__(image = Stone.image,
-                                    x = x, y = y, dy = Stone.speed)
-
-        self.time_till_drop = 0
-
-    def update(self):
-        self.collision()
-        self.check_fall()
-        self.check_drop()
 
     def collision(self):
         """ Sprawdź czy kamień dotknął gracza """
         for stone in self.overlapping_sprites:
             stone.handle_hit()
 
+
+class Stone(games.Sprite):
+    """ Kamień który spada na ziemię """
+
+    image = games.load_image("kamien.bmp")
+    speed = 2
+
+    def __init__(self, x = random.randrange(0, games.screen.width), y = 0):
+        super(Stone, self).__init__(image = Stone.image,
+                                    x = x, y = y, dy = Stone.speed)
+        self.time_til_drop = 20
+
+    def update(self):
+        self.check_fall()
+        self.check_drop()
+
+
     def check_fall(self):
         """ Sprawdź czy kamień dotknął ziemi """
         if self.bottom > games.screen.height:
-            Player.add_points(self)
             self.destroy()
 
-    def handle_hit(self):
-        """ Zniszcz gracza jeśli został trafiony """
-        Player.destroy(self)
-        self.end_game()
+    def check_drop(self):
+        """ Zrzut kamieni """
+        if self.time_til_drop > 0:
+            self.time_til_drop -= 1
+        else:
+            new_stone = Stone(x = random.randrange(0, games.screen.width))
+            games.screen.add(new_stone)
 
+            # ustaw margines na mniej więcej 30% wysokości kamienia, niezależnie od prędkości   
+            self.time_til_drop = int(new_stone.height * 50 / self.speed) + 50     
+
+            
     def end_game(self):
         """ Zakończ grę. """
         end_message = games.Message(value = "Koniec gry",
@@ -78,31 +77,26 @@ class Stone(games.Sprite):
                                     y = games.screen.height/2,
                                     lifetime = 5 * games.screen.fps,
                                     after_death = games.screen.quit)
-        games.screen.add(end_message)    
-    
-    def check_drop(self):
-        """ Zrzut kamieni """
-        if self.time_till_drop > 0:
-            self.time_till_drop -= 1
-        else:
-            new_stone = Stone(x = self.x)
-            games.screen.add(new_stone)
+        games.screen.add(end_message)      
 
-            # ustaw margines na mniej więcej 30% wysokości kamienia, niezależnie od prędkości   
-            self.time_till_drop = int(new_stone.height * 50 / Stone.speed) + 50  
-
+    def handle_hit(self):
+        """ Zniszcz gracza jeśli został trafiony """   
+        Player.destroy(self)     
+        self.end_game()
+   
+ 
 def main():
     """ Uruchom grę """
     wall_image = games.load_image("sciana.jpg", transparent = False)
     games.screen.background = wall_image
 
-    new_stone = Stone()
-    games.screen.add(new_stone)
-
     ludzik = Player()
     games.screen.add(ludzik)
 
-    games.mouse.is_visible - False
+    new_stone = Stone()
+    games.screen.add(new_stone)
+
+    games.mouse.is_visible = False
 
     games.screen.event_grab = True
     games.screen.mainloop()
