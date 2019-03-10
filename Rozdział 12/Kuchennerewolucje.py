fc75c3e72dd35b1420c10c4d004a7c66bb7a6fca
Konrad Lippert <45361699+grokoko@users.noreply.github.com>
2019-03-10 12:21:42 +0100

    Add files via upload

--- Rozdział 12/Kuchennerewolucje.py
+++ Rozdział 12/Kuchennerewolucje.py
@@ -38,7 +38,7 @@ class Kucharz(games.Sprite):
 class Ship(games.Sprite):
     image = games.load_image("statek.bmp")
     sound = games.load_sound("przyspieszenie.wav")
-    VELOCITY = 1
+    VELOCITY = 0.2
 
     def update(self): 
         if games.keyboard.is_pressed(games.K_LEFT):
@@ -47,10 +47,47 @@ class Ship(games.Sprite):
         if games.keyboard.is_pressed(games.K_RIGHT):
             Ship.sound.play()
             self.dx += Ship.VELOCITY
+
         if self.left < 0:
             self.left = 0
         if self.right > games.screen.width:
             self.right = games.screen.width
+
+        if games.keyboard.is_pressed(games.K_SPACE):
+            new_missile = Missile(self.x, self.y)
+            games.screen.add(new_missile)
+
+class Missile(games.Sprite):
+    image = games.load_image("pocisk.bmp")
+    sound = games.load_sound("pocisk.wav")
+    BUFFER = 40
+    LIFETIME = 100
+    MIS_VELOCITY = 0.1
+
+    def __init__(self, ship_x, ship_y):
+        Missile.sound.play()
+
+        buffer_x = Missile.BUFFER
+        buffer_y = Missile.BUFFER
+
+        x = ship_x + buffer_x
+        y = ship_y + buffer_y
+
+        dy = Missile.MIS_VELOCITY
+
+        super(Missile, self).__init__(image = Missile.image,
+                                        x = x, y = y,
+                                        dy = dy)
+        self.lifetime = Missile.LIFETIME
+
+    def update(self):
+        self.lifetime -= 1
+        if self.lifetime == 0:
+            self.destroy()
+
+        if self.top > 0:
+            self.destroy()
+
         
 def main():
     nebula_image = games.load_image("mglawica.jpg")
